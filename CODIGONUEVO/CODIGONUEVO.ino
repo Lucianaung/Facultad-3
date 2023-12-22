e6546b4419576371aa5e83aa40f7cd7ae6689e8f
Lucianaung <83373548+Lucianaung@users.noreply.github.com>
2023-12-22 20:46:24 -0300

    Add files via upload

---
+++ CODIGONUEVO/CODIGONUEVO.ino
@@ -0,0 +1,52 @@
+int trigPin = 9;
+int echoPin = 10;
+int led = 5;
+
+long duracion;
+int distancia;
+int intensidad;
+
+bool rayoActivo = false;
+long tiempoInicioRayo = 0;
+long duracionRayo = 3000;  //duración del rayo en milisegundos
+
+void setup() {
+  // inicializo los pines
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+
+  intensidad = 0;
+}
+
+void loop() {
+  //encender el sensor, por pulsos de 10 microsegundos
+  digitalWrite(trigPin, LOW);   // Nos aseguramos de que el trigger está desactivado
+  delayMicroseconds(2);         // Para asegurarnos de que el trigger esta LOW
+  digitalWrite(trigPin, HIGH);  // Activamos el pulso de salida
+  delayMicroseconds(10);        // Esperamos 10µs. El pulso sigue active este tiempo
+  digitalWrite(trigPin, LOW);   // Cortamos el pulso y a esperar el echo
+
+  duracion = pulseIn(echoPin, HIGH);
+  distancia = (duracion * 0.034) / 2;
+  delay(25);
+
+  if (!rayoActivo) {
+    int intensidad = map(distancia, 200, 100, 0, 255);  //CHEACKEAR LA DISTANCIAS
+    intensidad = constrain(intensidad, 0, 255);
+
+    analogWrite(led, intensidad);
+    if (distancia <= 90) {  //CHECKEAR LA DISTANCIA ACORDE
+      rayoActivo = true;
+      tiempoInicioRayo = millis();
+    }
+  } else {
+    long tiempoActual = millis();
+    if (tiempoActual - tiempoInicioRayo <= duracionRayo) {
+      analogWrite(led, intensidad( random (0, 255));
+      delay(random(5,15));
+    }
+  }
+}
